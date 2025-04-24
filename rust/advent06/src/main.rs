8e1aab7fb2eaeb469d5d3fb3a6759cf235c26770
Merschel, Steve <merschel@gmail.com>
2025-04-23 22:06:17 -0600

    Got 6p2 running in Rust

--- rust/advent06/src/main.rs
+++ rust/advent06/src/main.rs
@@ -46,9 +46,111 @@ fn main() {
     dev_print!("Number of X's: {}", count_xs(&map));
     print!("Number of X's: {}", count_xs(&map));
     println!();
-    
+
+    // part two
+    // reset variables
+    let (startrow, startcol, startdir) = find_start(&grid);
+    let mut row = startrow;
+    let mut col = startcol;
+    let mut dir = startdir;
+    let mut map2 = grid.clone();
+
+    // make an empty vector to track row and columns of added obstacles
+    let mut obstacles: Vec<(usize, usize)> = Vec::new();
+    loop {
+        // if not next to exit (we can't add an obstacle at the edge of the map)
+        if !at_exit(row, col, map2.len(), map2[0].len(), dir) {
+            // add "O" in front of current position
+            dev_print!("Adding obstacle at ({}, {})", col, row);
+            let (obsrow, obscol) = add_obstacle(&mut row, &mut col, &mut dir, &mut map2);
+            // run sim
+            let mut simrow = startrow;
+            let mut simcol = startcol;
+            let mut simdir = startdir;
+            if run_simulation(&mut simrow, &mut simcol, &mut simdir, &mut map2) {
+                // append obsrow and obscol to obstacles, if it doesn't already exist
+                if !obstacles.contains(&(obsrow, obscol)) {
+                    obstacles.push((obsrow, obscol));
+                }
+            }
+            // remove "O" in front of current position
+            dev_print!("Removing obstacle at ({}, {})", col, row);
+            remove_obstacle(&mut row, &mut col, &mut dir, &mut map2);
+        } else {
+            break;
+        }
+
+        // move in the current direction
+        do_move(&mut row, &mut col, &mut dir, &mut map2);
+    }
+    // print the number of obstacles
+    print!("Number of obstacles: {}", obstacles.len());
+
 }
 
+fn add_obstacle(row: &mut usize, col: &mut usize, dir: &mut Direction, map: &mut Vec<Vec<char>>) -> (usize, usize) {
+    // don't add one if there is already one there
+    if !check_obstacle(row, col, dir, map, '#') {
+        if !check_obstacle(row, col, dir, map, '^') {
+            // add "O" in front of current position
+            match dir {
+                Direction::Up => { 
+                    map[*row - 1][*col] = 'O';
+                    return (*row - 1, *col)
+                },
+                Direction::Right => { 
+                    map[*row][*col + 1] = 'O';
+                    return (*row, *col + 1)
+                },
+                Direction::Down => { 
+                    map[*row + 1][*col] = 'O';
+                    return (*row + 1, *col)
+                },
+                Direction::Left => { 
+                    map[*row][*col - 1] = 'O';
+                    return (*row, *col - 1)
+                },
+            }
+        }
+    }
+    return (0,0);
+}
+
+fn remove_obstacle(row: &mut usize, col: &mut usize, dir: &mut Direction, map: &mut Vec<Vec<char>>) {
+    // don't remove it if we didn't add it
+    if check_obstacle(row, col, dir, map, 'O') {
+        // remove "O" in front of current position
+        match dir {
+            Direction::Up => map[*row - 1][*col] = '.',
+            Direction::Right => map[*row][*col + 1] = '.',
+            Direction::Down => map[*row + 1][*col] = '.',
+            Direction::Left => map[*row][*col - 1] = '.',
+        }
+    }
+}
+
+fn run_simulation(row: &mut usize, col: &mut usize, dir: &mut Direction, map: &mut Vec<Vec<char>>) -> bool {
+    let mut step_count = 0;
+    // make a copy of the map
+    let mut simmap = map.clone();
+    // run the simulation until we hit an obstacle
+    loop {
+        // check for exit
+        if at_exit(*row, *col, simmap.len(), simmap[0].len(), *dir) {
+            return false;
+        }
+
+        // move in the current direction
+        do_move(row, col, dir, &mut simmap);
+
+        step_count += 1;
+
+        if step_count > (simmap.len() * simmap[0].len()) {
+                // if we have moved more than the number of cells in the map, we are in an infinite loop
+            return true;
+        }
+    }
+}
 
 /// Reads an input file and converts it to a 2D array of characters
 ///
@@ -113,38 +215,33 @@ fn do_move(row: &mut usize, col: &mut usize, dir: &mut Direction, map: &mut Vec<
     // print column and row
     dev_print!("Current position: ({}, {})", col, row);
     // check for # in front of current position
+    if check_obstacle(row, col, dir, map, '#') {
+        turn_right(dir);
+    } else if check_obstacle(row, col, dir, map, 'O') {
+        // if we are next to an obstacle, turn right
+        turn_right(dir);
+    } else {
+        move_forward(row, col, dir);
+    }
+}
+
+fn check_obstacle(row: &usize, col: &usize, dir: &Direction, map: &Vec<Vec<char>>, symbol: char) -> bool {
+    // check for # in front of current position
     match dir {
-        Direction::Up => {
-            // print map[*col][*row - 1]
-            dev_print!("Map: {} ", map[*col][*row - 1]);
-            dev_print!("Map: {} ", map[*row-1][*col]);
-            if map[*row-1][*col] == '#' {
-                turn_right(dir);
-            } else {
-                *row -= 1;
-            }
-        }
-        Direction::Right => {
-            if map[*row][*col+1] == '#' {
-                turn_right(dir);
-            } else {
-                *col += 1;
-            }
-        }
-        Direction::Down => {
-            if map[*row+1][*col] == '#' {
-                turn_right(dir);
-            } else {
-                *row += 1;
-            }
-        }
-        Direction::Left => {
-            if map[*row][*col-1] == '#' {
-                turn_right(dir);
-            } else {
-                *col -= 1;
-            }
-        }
+        Direction::Up => map[*row - 1][*col] == symbol,
+        Direction::Right => map[*row][*col + 1] == symbol,
+        Direction::Down => map[*row + 1][*col] == symbol,
+        Direction::Left => map[*row][*col - 1] == symbol,
+    }
+}
+
+fn move_forward(row: &mut usize, col: &mut usize, dir: &mut Direction) {
+    // move in the current direction
+    match dir {
+        Direction::Up => *row -= 1,
+        Direction::Right => *col += 1,
+        Direction::Down => *row += 1,
+        Direction::Left => *col -= 1,
     }
 }
 
